38298afff782d19c01d6d3bc3c2f52a6a6fb2317
schattaraj <schattaraj200@gmail.com>
2026-05-03 12:22:45 +0530

    update

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -42,7 +42,7 @@ export function useProfile() {
           const res = await fetch(
             `${process.env.NEXT_PUBLIC_API_URL}profile`,
             {
-              credentials: "include",
+            //   credentials: "include",
               headers: {
                 "Content-Type": "application/json",
                 Authorization: token ? `Bearer ${token}` : "",
